dac1019df81dd10354f671248f563420d504982a
Martin <martymcfly333@gmail.com>
2024-01-12 15:57:16 +0000

    first steps

--- ckanext/glab/plugin.py
+++ ckanext/glab/plugin.py
@@ -2,13 +2,11 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class GlabPlugin(plugins.SingletonPlugin):
+class MyCoolPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'glab')
+        toolkit.add_template_directory(config_, 'templates/cool_temp')
+        toolkit.add_public_directory(config_, 'public/cool_static')
--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        glab=ckanext.glab.plugin:GlabPlugin
+        glab=ckanext.glab.plugin:MyCoolPlugin
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
